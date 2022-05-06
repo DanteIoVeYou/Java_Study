8172bfd376d797e3b5796a020b2084f8110d196e
danteking <danteking@aliyun.com>
2022-05-06 14:00:35 +0800

    lesson1

---
+++ 2022_5_6_初始java数据类型/HelloWorld.java
@@ -0,0 +1,13 @@
+
+
+public class HelloWorld {
+	static int x;
+	public static void main(String[] args) {
+		int a = Integer.MAX_VALUE;
+		System.out.println(a+1);
+		System.out.println(Integer.MIN_VALUE-1);
+		System.out.println(x);
+		System.out.println("HelloWorld");
+	}
+}
+
